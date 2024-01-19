cf3ada72d03023cc3b3c6ea72bd2646dae3574f3
Yayat Nurhidayat <yayatnhtedu@gmail.com>
2024-01-19 16:01:37 +0700

    Initial Commit

---
+++ SwiftExploration-SidatluUITests/SwiftExploration_SidatluUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftExploration_SidatluUITestsLaunchTests.swift
+//  SwiftExploration-SidatluUITests
+//
+//  Created by Yayat Nurhidayat on 19/01/24.
+//
+
+import XCTest
+
+final class SwiftExploration_SidatluUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
